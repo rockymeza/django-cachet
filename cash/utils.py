adb47ef0b86fc1d6bbed4f0a4d05b66ff0003b96
Rocky Meza <rocky@fusionbox.com>
2013-08-18 07:06:40 -0600

    Fixed Django < 1.5 compatibility

--- cash/utils.py
+++ cash/utils.py
@@ -3,8 +3,14 @@ from __future__ import absolute_import, unicode_literals
 # Based on django.core.cache.utils for compatibility with < Django 1.6
 
 import hashlib
-from django.utils.encoding import force_bytes
+import itertools
+
 from django.utils.http import urlquote
+
+try:
+    from django.utils.encoding import force_bytes
+except ImportError:  # Django < 1.5 (which means Python < 3)
+    force_bytes = str
 
 TEMPLATE_FRAGMENT_KEY_TEMPLATE = 'template.cash.%s'
 
@@ -12,6 +18,7 @@ TEMPLATE_FRAGMENT_KEY_TEMPLATE = 'template.cash.%s'
 def make_template_fragment_key(fragment, vary_on=None):
     if vary_on is None:
         vary_on = ()
-    key = ':'.join([fragment] + [urlquote(var) for var in vary_on])
+    key_args = itertools.chain((fragment,), vary_on)
+    key = ':'.join([urlquote(var) for var in key_args])
     args = hashlib.md5(force_bytes(key))
     return TEMPLATE_FRAGMENT_KEY_TEMPLATE % args.hexdigest()
